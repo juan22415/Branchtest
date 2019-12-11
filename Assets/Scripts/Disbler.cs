02d669b1c3690639f131aa43f2596fecc3d7f56c
juanriosq <camilo.rios@wizard.com.co>
2019-12-10 19:21:58 -0500

    Repo Created

---
+++ Assets/Scripts/Disbler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disbler : MonoBehaviour
+{
+
+/// <summary>
+/// Start is called on the frame when a script is enabled just before
+/// any of the Update methods is called the first time.
+/// </summary>
+void Start()
+{
+    
+}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<MeshRenderer>().enabled = !GetComponent<MeshRenderer>().enabled;
+        }
+    }
+}
